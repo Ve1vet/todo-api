e9c27b37542e2e6ed0c27adbac8250004a9d43cf
Velvet <vovk.work@gmail.com>
2020-09-05 05:25:30 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ let app = express();
 
 app.use( bodyParser.json() );
 
+app.get( '/todos', ( req, res ) => {
+
+  ToDoModel.find().then( todos => {
+
+    res.send( {todos} );
+  }, e => {
+    res.status( 400 ).send( e );
+  } );
+} );
+
 app.listen( 3000, () => {
   console.log( `Started on port 3000` );
 } );
